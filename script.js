ca0e3694da09a30b8cc2d92957ed264cee6076c5
rncraven94 <neilcraven2013@gmail.com>
2023-07-06 16:02:40 -0400

    index.html

--- script.js
+++ script.js
@@ -10,16 +10,33 @@ const btnLeft = document.querySelector(".slider_btn-left");
 const btnRight = document.querySelector(".slider_btn-right");
 let curSlide = 0;
 const maxSlide = slides.length;
-slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+const imgHidden = document.getElementById("book-img");
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
 
 // going to the next slide
-btnRight.addEventListener("click", function () {
+const nextSlide = function () {
   if (curSlide === maxSlide - 1) {
     curSlide = 0;
   } else {
     curSlide++;
   }
-  slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
-  );
-});
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
